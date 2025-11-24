43f8044a87e16bec55d52bc59fbe9d64b4da2290
logapriyanm <logapriyanvky@gmail.com>
2025-11-24 12:53:03 +0530

    Latest

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -66,6 +66,8 @@ const adminLogin = async (req, res) => {
   try {
     const { email, password } = req.body;
 
+
+
     if (
       email === process.env.ADMIN_EMAIL &&
       password === process.env.ADMIN_PASSWORD
